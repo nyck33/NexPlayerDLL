110f11c83b537ffe053583da1d6f686f78083dd3
Nobutaka Kim <nobutaka@gatech.edu>
2022-08-13 00:13:06 -0600

    tried using a class but requires instance parameter so use global variables

--- Header.h
+++ Header.h
@@ -1,2 +1,59 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
+#pragma once
+
+#ifdef NEXPLAYERDLL_EXPORTS
+#	define EXPORT __declspec(dllexport)
+#else
+#define EXPORT __declspec(dllimport)
+#endif
+
+class PauseResumeTracker {
+
+public:
+	int onPlayPauseTime;
+	int lastPauseResumed;
+	int numPauseResumed;
+	enum class playState { waiting = 0, paused, playing };
+	playState state;
+
+	void setLastPR(int timestamp) {
+		lastPauseResumed = timestamp;
+	}
+	
+	int getLastPR(){
+		return lastPauseResumed;
+	}
+	
+	void incrementNumPR() {
+		if (numPauseResumed == NULL) {
+			numPauseResumed = 0;
+		}
+		else {
+			numPauseResumed++;
+		}
+	}
+	
+	int getNumPR() {
+		return numPauseResumed;
+	}
+
+	//parameters/ return values for playState are ints
+	//so they can be called in C#
+	void setPlayState(int newState){
+		//newState is 0/1/2
+		state = static_cast<playState>(newState);
+	}
+
+	int getPlayState() {
+		return static_cast<int>(state);
+	}
+
+	//int Foo(int a); //placeholder
+};
+
+extern "C" EXPORT PauseResumeTracker* createNewPauseResumeTracker();
+extern "C" EXPORT void OnPlayPause(int current_playback_time);
+extern "C" EXPORT int GetNubmerOfPlayPauseEvents();
+extern "C" EXPORT int GetLastPlayPauseTimestamp();
+
 
--- dllmain.cpp
+++ dllmain.cpp
@@ -1,4 +1,97 @@
+#include "pch.h"
+//#include <utility>
+//#include <limits.h>
+#include "Header.h"
 
+void PauseResumeTracker::setLastPR(int timestamp) {
+    /*
+    * transform int timestamp to int
+    */
+	lastPauseResumed = timestamp;
+}
+
+int PauseResumeTracker::getLastPR() {
+	return lastPauseResumed;
+}
+
+void PauseResumeTracker::incrementNumPR() {
+	if (numPauseResumed == NULL) {
+		numPauseResumed = 0;
+	}
+	else {
+		numPauseResumed++;
+	}
+}
+
+int PauseResumeTracker::getNumPR() {
+	return numPauseResumed;
+}
+
+//parameters/ return values for playState are ints
+//so they can be called in C#
+void PauseResumeTracker::setPlayState(int newState) {
+	//newState is 0/1/2
+	state = static_cast<playState>(newState);
+}
+
+int PauseResumeTracker::getPlayState() {
+	return static_cast<int>(state);
+}
+
+//in lieu of constructor
+EXPORT PauseResumeTracker* createNewPauseResumeTracker() {
+    PauseResumeTracker* pauseResumeTracker = new PauseResumeTracker();
+    pauseResumeTracker->setLastPR(0);
+    pauseResumeTracker->incrementNumPR();
+    pauseResumeTracker->setPlayState(0);
+    
+    return pauseResumeTracker;
+}
+
+EXPORT void OnPlayPause(PauseResumeTracker* instance, int current_playback_time) {
+    /*timestamp of video time when player was last paused 
+    / resumed
+    */
+    switch (instance->state) {
+    case PauseResumeTracker::playState::waiting: {
+        instance->setPlayState(2);
+        instance->numPauseResumed++;
+        instance->onPlayPauseTime = current_playback_time;
+        break;
+    }
+    case PauseResumeTracker::playState::paused: {
+        instance->setPlayState(2);
+        instance->numPauseResumed++;
+        instance->onPlayPauseTime = current_playback_time;
+        break;
+    }
+    case PauseResumeTracker::playState::playing: {
+        instance->setPlayState(1);
+        instance->numPauseResumed++;
+        instance->onPlayPauseTime = current_playback_time;
+        break;
+    }
+        
+
+    }
+
+}
+
+int GetNumberOfPlayPauseEvents(PauseResumeTracker* instance) {
+    /*
+    * return num times player has been paused / resumed
+    state machine for paused, playing, 
+    */
+    
+    return instance->getNumPR();
+}
+
+int GetLastPlayPauseTimestamp() {
+    /*
+    * return the last play
+    */
+    return 0;
+}
 
 
 
